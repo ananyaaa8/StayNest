0f585d3d0e4c9d8e1081118ea278f13d060482bd
Ananya Gupta <ananya2004gupta@gmail.com>
2025-10-01 21:10:50 +0530

    add new updates

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -3,15 +3,38 @@ const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const mapToken = process.env.MAP_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
+// INDEX (with search functionality)
 module.exports.index = async (req, res) => {
-    const allListings = await listing.find({});
-    res.render("./listings/index.ejs", { allListings });
-}
+    const { search } = req.query;
+    let allListings;
 
+    if (search && search.trim() !== "") {
+        const regex = new RegExp(search, "i"); // case-insensitive search
+        allListings = await listing.find({
+            $or: [
+                { title: regex },
+                { location: regex },
+                { country: regex }
+            ]
+        });
+
+        if (allListings.length === 0) {
+            req.flash("error", "No listings found matching your search.");
+            return res.redirect("/listings");
+        }
+    } else {
+        allListings = await listing.find({});
+    }
+
+    res.render("./listings/index.ejs", { allListings, search });
+};
+
+// NEW FORM
 module.exports.renderNewForm = (req, res) => {
     res.render("./listings/new.ejs");
 };
 
+// SHOW LISTING
 module.exports.showListing = async (req, res, next) => {
     let { id } = req.params;
     const Listing = await listing.findById(id).populate({
@@ -20,20 +43,20 @@ module.exports.showListing = async (req, res, next) => {
             path: "author",
         },
     }).populate("owner");
+
     if (!Listing) {
         req.flash("error", "Listing you requested for doesn't exist");
-        res.redirect("/listings");
-        return;
+        return res.redirect("/listings");
     }
     res.render("./listings/show.ejs", { Listing, mapToken: process.env.MAP_TOKEN });
 };
 
+// CREATE LISTING
 module.exports.createListing = async (req, res) => {
     let response = await geocodingClient.forwardGeocode({
         query: req.body.listing.location,
         limit: 1
-    })
-        .send();
+    }).send();
 
     let url = req.file.path;
     let filename = req.file.filename;
@@ -42,17 +65,18 @@ module.exports.createListing = async (req, res) => {
     newListing.image = { url, filename };
     newListing.geometry = response.body.features[0].geometry;
     await newListing.save();
+
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
 };
 
+// EDIT FORM
 module.exports.renderEditForm = async (req, res) => {
-
     let { id } = req.params;
     const Listing = await listing.findById(id);
     if (!Listing) {
         req.flash("error", "Listing doesn't exist!");
-        res.redirect("/listing");
+        return res.redirect("/listings");
     }
 
     let originalUrl = Listing.image.url;
@@ -60,8 +84,8 @@ module.exports.renderEditForm = async (req, res) => {
     res.render("./listings/edit.ejs", { Listing, newUrl });
 };
 
+// UPDATE LISTING
 module.exports.updateListing = async (req, res) => {
-
     let { id } = req.params;
     let Listing = await listing.findByIdAndUpdate(id, { ...req.body.listing });
     if (typeof req.file !== "undefined") {
@@ -72,12 +96,13 @@ module.exports.updateListing = async (req, res) => {
     }
     req.flash("success", "Listing updated!");
     res.redirect(`/listings/${id}`);
-}
+};
 
+// DELETE LISTING
 module.exports.deleteListing = async (req, res) => {
     let { id } = req.params;
-    let deletedlListing = await listing.findByIdAndDelete(id);
-    console.log(deletedlListing);
+    let deletedListing = await listing.findByIdAndDelete(id);
+    console.log(deletedListing);
     req.flash("success", "Listing deleted!");
     res.redirect("/listings");
-}
+};
--- routes/listingRoute.js
+++ routes/listingRoute.js
@@ -1,3 +1,50 @@
+// const express = require("express");
+// const router = express.Router();
+// const wrapAsync = require("../utils/wrapAsync");
+// const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+// const listingController = require("../controllers/listingController.js");
+// const { storage } = require("../cloudConfig.js");
+// const multer = require("multer");
+// const upload = multer({ storage });
+
+// router.route("/")
+//     .get(wrapAsync(listingController.index))
+//     .post(isLoggedIn, upload.single('listing[image]'), validateListing, wrapAsync(listingController.createListing));
+
+
+// // new route
+// router.get("/new", isLoggedIn, listingController.renderNewForm);
+
+
+// router.route("/:id")
+//     .get(wrapAsync(listingController.showListing))
+//     .put(isLoggedIn,upload.single('listing[image]') ,validateListing, isOwner, wrapAsync(listingController.updateListing))
+//     .delete(isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
+
+
+// // index route
+// // router.get("/",wrapAsync(listingController.index));
+
+
+// // show route
+// // router.get("/:id", wrapAsync(listingController.showListing));
+
+// // create route
+// // router.post("/", validateListing, isLoggedIn, wrapAsync(listingController.createListing));
+
+// // edit route
+// router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
+
+// // update route
+// // router.put("/:id", validateListing, isLoggedIn, isOwner, wrapAsync(listingController.updateListing));
+
+// // delete route
+// // router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
+
+
+// module.exports = router;
+
+
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync");
@@ -7,39 +54,20 @@ const { storage } = require("../cloudConfig.js");
 const multer = require("multer");
 const upload = multer({ storage });
 
+// Index + Search Route
 router.route("/")
     .get(wrapAsync(listingController.index))
     .post(isLoggedIn, upload.single('listing[image]'), validateListing, wrapAsync(listingController.createListing));
 
-
-// new route
+// New route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
-
 
 router.route("/:id")
     .get(wrapAsync(listingController.showListing))
-    .put(isLoggedIn,upload.single('listing[image]') ,validateListing, isOwner, wrapAsync(listingController.updateListing))
+    .put(isLoggedIn, upload.single('listing[image]'), validateListing, isOwner, wrapAsync(listingController.updateListing))
     .delete(isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
 
-
-// index route
-// router.get("/",wrapAsync(listingController.index));
-
-
-// show route
-// router.get("/:id", wrapAsync(listingController.showListing));
-
-// create route
-// router.post("/", validateListing, isLoggedIn, wrapAsync(listingController.createListing));
-
-// edit route
+// Edit route
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
-
-// update route
-// router.put("/:id", validateListing, isLoggedIn, isOwner, wrapAsync(listingController.updateListing));
-
-// delete route
-// router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
-
 
 module.exports = router;
